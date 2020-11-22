9c1293807a94f987a6ae81670609b3ed987b4a7d
jonkarrer <karrerjon@gmail.com>
2020-11-21 22:23:58 -0600

    RoughDraft 1

---
+++ script.js
@@ -0,0 +1,27 @@
+class Player {
+    constructor() {
+        this.name = name;
+        this.X = false;
+        this.O = false;
+    }
+    placeMarker() {
+        //Click element
+        
+
+    }
+}
+class Board {
+    constructor(){
+        this.row1 = [];
+        this.row2 = [];
+        this.row3 = [];
+    }
+    declareWinner(){
+
+    }
+}
+
+var pick = document.querySelector('#midMid');
+pick.innerText = "X"
+
+
